ca09d0c2d0a3427b91bb0bcf48f895f5ebf45493
Tim Toulas <tim.toulas@liip.ch>
2016-09-09 16:19:11 +0200

    Flask platform.sh example initial commit

---
+++ setup.py
@@ -0,0 +1,11 @@
+from distutils.core import setup
+
+setup(name='Distutils',
+      version='1.0',
+      description='Flask platform.sh example',
+      author='Tim Toulas',
+      author_email='tim.toulas@...',
+      url='https://.../',
+      packages=['distutils', 'distutils.command'],
+     )
+
